b2b858e68dc137b7768027b0e50b131342a71ce6
Abdullah Mansoor <abdullah.mansoor@osos.om>
2024-05-27 23:31:47 +0530

    build!: update the angular version v18

--- projects/typewriter/src/lib/typewriter.component.ts
+++ projects/typewriter/src/lib/typewriter.component.ts
@@ -1,21 +1,24 @@
-import { Component, Input, OnInit, computed, signal } from '@angular/core';
+import {Component, Input, OnInit, computed, signal, input} from '@angular/core';
 
 @Component({
   selector: 'ngx-typewriter',
   standalone: true,
-  template: ` <span [className]="panelClass">{{ text() }}</span> `,
+  template: ` <span [className]="panelClass()">{{ text() }}</span> `,
 })
 export class Typewriter implements OnInit {
-  @Input() words = ['Hello World!', 'This is', 'a simple Typewriter'];
-  @Input() typeSpeed = 80;
-  @Input() deleteSpeed = 50;
-  @Input() panelClass = 'test';
+  words = input<string[]>(['Hello World!', 'This is', 'a simple Typewriter']);
+  typeSpeed = input(80);
+  deleteSpeed = input(50);
+  panelClass = input('');
+  loop = input(0);
 
-  wordIndex = signal(0);
-  word = computed(() => this.words[this.wordIndex()]);
+  private wordIndex = signal(0);
+  private word = computed(() => this.words()[this.wordIndex()]);
 
-  textIndex = signal(0);
-  text = computed(() => this.word().slice(0, this.textIndex()));
+  private textIndex = signal(0);
+  protected text = computed(() => this.word().slice(0, this.textIndex()));
+
+  private currentLoopCount = signal(0);
 
   ngOnInit(): void {
     this.type();
@@ -28,18 +31,28 @@ export class Typewriter implements OnInit {
         clearInterval(int);
         this.delete();
       }
-    }, this.typeSpeed);
+    }, this.typeSpeed());
   }
 
   delete() {
     const int = setInterval(() => {
       this.textIndex.update((v) => v - 1);
       if (this.textIndex() < 0) {
-        this.wordIndex.update((v) => (v + 1) % this.words.length);
+        this.wordIndex.update((v) => (v + 1) % this.words().length);
         clearInterval(int);
         this.textIndex.set(0);
-        this.type();
+        if (this.loop() > 0) {
+          this.currentLoopCount.update((v) => v + 1);
+          if (this.currentLoopCount() < this.loop()) {
+            this.type();
+          } else {
+            this.wordIndex.set(this.words().length - 1);
+            this.textIndex.set(this.word().length);
+          }
+        } else {
+          this.type();
+        }
       }
-    }, this.deleteSpeed);
+    }, this.deleteSpeed());
   }
 }
